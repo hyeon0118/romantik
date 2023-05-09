9d789db94e4a8f8c9d499bbe21a16a882842898c
hyeon0118 <hyeon0118@gmail.com>
2023-05-09 13:37:17 +0200

    add iframe,  play/pause function

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,18 +8,22 @@ const playerHeader = document.querySelector(".player-header");
 const bottomIcons = document.querySelector(".bottom-icons");
 const viewPlaylist = document.querySelector(".controller-right-wrapper.desktop img:nth-of-type(3)")
 const playlistClose = document.querySelector(".current-playlist .player-header img");
+const nowPlaying = document.querySelector(".now-playing")
 
-player.addEventListener("click", () => {
-    if (window.innerWidth >= 600) {
-        body.classList.toggle("not-overflowY");
-        currentPlaylist.classList.toggle("hidden");
-    } else if (window.innerWidth < 600) {
+nowPlaying.addEventListener("click", () => {
+    // if (window.innerWidth >= 600) {
+    //     body.classList.toggle("not-overflowY");
+    //     currentPlaylist.classList.toggle("hidden");
+    // } 
+    if (window.innerWidth < 600) {
         player.classList.add("open");
         body.classList.add("not-overflowY");
         playerHeader.classList.remove("hidden");
         bottomIcons.classList.remove("hidden");
     }
 });
+
+
 
 playlistClose.addEventListener("click", () => {
     body.classList.remove("not-overflowY");
@@ -51,3 +55,81 @@ if (currentPath !== "/profile") {
     let menuItem = document.querySelector('[href="' + currentPath + '"]').parentNode;
     menuItem.classList.add('active');
 }
+
+
+
+
+const playButton = document.querySelectorAll(".play")
+const iframe = document.querySelector("#iframe")
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var iframePlayer;
+let currentVideoId = ""
+function onYouTubeIframeAPIReady() {
+    iframePlayer = new YT.Player('player', {
+        height: '0',
+        width: '0',
+        videoId: '06-puNWFuMQ',
+        // videoId: currentVideoId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+let isPlaying = false;
+
+function playPause() {
+    if (isPlaying) {
+        playButton.forEach(btn => {
+            btn.className = "pause"
+            btn.src = "static/icons/pause.svg";
+        })
+    } else if (!isPlaying) {
+        playButton.forEach(btn => {
+            btn.className = "play"
+            btn.src = "static/icons/play.svg";
+        })
+    }
+}
+
+
+
+
+function onPlayerReady(event) {
+    console.log("hi");
+    playButton.forEach(btn => {
+        btn.addEventListener('click', () => {
+            iframePlayer.playVideo();
+            isPlaying = !isPlaying;
+            if (isPlaying) {
+                iframePlayer.playVideo();
+            } else {
+                iframePlayer.pauseVideo();
+            }
+            playPause();
+        });
+    });
+}
+
+
+
+let login = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && login == false) {
+        setTimeout(stopVideo, 60000);
+    }
+}
+
+
+function stopVideo() {
+    iframePlayer.stopVideo();
+}
+
